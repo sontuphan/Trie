96bfa5bd4911bdc274da81ca50fe82106349ee1d
phansontu <sontu@bacoor.co>
2018-02-23 14:13:46 +0700

    network commands, git ignore

---
+++ mainnet.sh
@@ -0,0 +1,1 @@
+geth --datadir "~/Library/Ethereum/mainnet" --rpc --rpcapi "eth,net,personal,web3" --rpcaddr "0.0.0.0" --rpccorsdomain "*" --ws --wsapi "eth,net,personal,web3" --wsorigins "0.0.0.0" --fast
---
+++ ropsten.sh
@@ -0,0 +1,1 @@
+geth --testnet --datadir "~/Library/Ethereum/ropsten" --rpc --rpcapi "eth,net,personal,web3" --rpcaddr "0.0.0.0" --rpccorsdomain "*" --ws --wsapi "eth,net,personal,web3" --wsorigins "0.0.0.0"
